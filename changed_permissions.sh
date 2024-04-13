c109669794a35a10f4bf95606e1ee098c91b959f
Parsa <pshey040@uottawa.ca>
2024-04-13 01:59:28 -0400

    please bro please man

--- changed_permissions.sh
+++ changed_permissions.sh
@@ -1,4 +1,18 @@
-#!/bin/sh 
+#!/bin/sh
 
+# Debugging message
+echo "Executing changed_permissions.sh script..."
 
+# Debugging message
+echo "Current directory: $(pwd)"
+
+# Attempt to change permissions
 chmod a+rx ./chest_xray_classification_model_20240407_071036/saved_model.pb
+
+# Check if the chmod command succeeded
+if [ $? -eq 0 ]; then
+    echo "Permissions changed successfully."
+else
+    echo "Failed to change permissions."
+    exit 1  # Exit with error status
+fi
